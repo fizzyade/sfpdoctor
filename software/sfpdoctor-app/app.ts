93bf29a484fb37024effe58d2a1d2d7d2f2bd49b
Adrian Carpenter <adrian@fizzyade.com>
2019-09-30 13:26:30 +0100

    wip: added GPL header to source files

--- software/sfpdoctor-app/app.ts
+++ software/sfpdoctor-app/app.ts
@@ -1,11 +1,27 @@
-//const { app, BrowserWindow } = require('electron')
+/*
+ * Copyright (C) 2019 Adrian Carpenter
+ *
+ * This file is part of SFP Doctor a hardware & software project for reading/writing SFP modules.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import { app, BrowserWindow } from "electron";
 import * as Usb from "usb" ;
 import { IDevice, isIDevice } from "./interfaces/IDevice";
 import { IObject, isIObject } from "./interfaces/IObject";
 import { IDeviceFactory, isIDeviceFactory } from "./interfaces/IDeviceFactory";
-
-//var usb = require('usb');
 
 // npm install electron --save-dev
 // npm install --save-dev electron-rebuild
